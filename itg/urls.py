ada124750f230d40d5b95cd4a7b7eec54f74fccb
Maks Iamchuk <maks.iamchuk@outlook.com>
2025-01-06 15:37:59 +0500

    Урок 3: создали шаблон nav_menu.html и подключили его в базовом шаблоне

--- itg/urls.py
+++ itg/urls.py
@@ -7,7 +7,7 @@ from news import views
 # Подключаем файл urls.py из приложения news через include
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.main),
-    path('about/', views.about),
+    path('', views.main, name='index'),
+    path('about/', views.about, name='about'),
     path('news/', include('news.urls')),
 ]
