c96a745c6a7650776543f12cd9a3e4fae0d255b6
Maks Iamchuk <maks.iamchuk@outlook.com>
2025-01-20 14:21:41 +0500

    Урок 13: изменили заголовки в административной панели

--- news/admin.py
+++ news/admin.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 from .models import Article, Category, Tag
 
 
+admin.site.site_header = "Info to Go Admin Portal"
+admin.site.site_title = "Info to Go Admin Portal"
+admin.site.index_title = "Welcome to ITG Admin Portal"
+
+
 admin.site.register(Article)
 admin.site.register(Category)
 admin.site.register(Tag)
