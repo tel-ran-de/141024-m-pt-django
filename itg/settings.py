51c227c7daf046f3077fffd65c7a0b7f8480e91e
Maks Iamchuk <maks.iamchuk@outlook.com>
2025-02-07 14:58:30 +0500

    Урок 27: авторизация опционально через email или username

--- itg/settings.py
+++ itg/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+from django.conf.global_settings import AUTHENTICATION_BACKENDS
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -206,3 +208,8 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 LOGIN_URL = 'users:login'
+# AUTHENTICATION_BACKENDS = [
+#     'django.contrib.auth.backends.ModelBackend',
+# ]  # стандартный бэкенд для аутентификации по username
+
+AUTHENTICATION_BACKENDS += ['users.authentication.EmailAuthBackend']
