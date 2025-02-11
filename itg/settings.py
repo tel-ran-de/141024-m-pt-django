1cc4a3dfe62be841259afb3b2d9f862eeb77d44d
Maks Iamchuk <maks.iamchuk@outlook.com>
2025-02-11 15:05:16 +0500

    Урок 29: оптимизировали код: удалили BaseMixin, добавили контекстный процессор и кэширование категорий

--- itg/settings.py
+++ itg/settings.py
@@ -111,6 +111,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'news.context_processors.global_context',
             ],
         },
     },
