9947cb289faed12c1b0fab4ad83868b8322ae448
Maks Iamchuk <maks.iamchuk@outlook.com>
2025-02-25 22:18:12 +0500

    Урок 33: добавили систему отслеживания изменений статей

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import redirect
 from django.views.generic import TemplateView, ListView
-from news.models import Article
+from news.models import Article, ArticleHistory
 
 from .forms import UserUpdateForm, ProfileUpdateForm
 
